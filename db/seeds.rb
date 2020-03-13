6d0697529f4ae4a6fb452780bc6235c1aa1b5bea
tantan <tantant5656@gmail.com>
2020-03-13 18:28:14 +0900

    タスク管理アプリ2出来ました

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,13 @@
+Task.create(
+  title: "test1",
+  body: "test1"
+)
+
+Task.create(
+  title: "test2",
+  body: "test2"
+)
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
